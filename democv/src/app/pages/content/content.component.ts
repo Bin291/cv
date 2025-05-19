1d2ecb03358c24011d64e3b74692845fc6bf0d83
TienSang <luutiensang06@gmail.com>
2025-05-19 18:57:27 +0700

    feat: from input in4

--- democv/src/app/pages/content/content.component.ts
+++ democv/src/app/pages/content/content.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {InputcontentComponent} from '../../components/inputcontent/inputcontent.component';
+import { EditDetailsComponent } from "../../components/edit-details/edit-details.component";
 
 
 @Component({
   selector: 'app-content',
   imports: [
-    InputcontentComponent
-  ],
+    InputcontentComponent,
+    EditDetailsComponent
+],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss'
 })
