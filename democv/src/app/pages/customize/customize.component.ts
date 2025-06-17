f9d402f8f5475791fbe74ded4159f0a1e4f35413
trunghoa456 <hoa.tt02008@sinhvien.hoasen.edu.vn>
2025-06-17 14:04:59 +0700

    a new file

--- democv/src/app/pages/customize/customize.component.ts
+++ democv/src/app/pages/customize/customize.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {MatButtonModule, MatIconButton} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatLabel} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+import { MatCardModule} from '@angular/material/card';
+import {NgForOf} from '@angular/common';
+
+
 
 @Component({
   selector: 'app-customize',
-  imports: [],
+  imports: [
+
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatCheckboxModule,
+    MatCardModule,
+
+  ],
   templateUrl: './customize.component.html',
   styleUrl: './customize.component.scss'
 })
